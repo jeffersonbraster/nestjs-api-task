59678660de708799e0112714e29090e2539bfacb
Jefferson <jeffersonbraster@gmail.com>
2020-11-17 11:26:41 -0300

    validando erro no delete task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -56,7 +56,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const taskById = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== taskById.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
