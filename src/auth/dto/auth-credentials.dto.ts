62e72fd268fe2879f7934608fe92ed604b9a2cc9
Jefferson <jeffersonbraster@gmail.com>
2020-11-17 22:46:08 -0300

    Fazendo validações na criação de users

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(10)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(10)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Precisa ter uma letra maiuscula, minuscula um caracter especial ou número.',
+  })
   password: string;
 }
